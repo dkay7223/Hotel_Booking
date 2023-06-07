5ad1d2a3c8fcac0074b2ee72d28e7307b213af9a
Daniyal <dkay7223@gmail.com>
2023-06-07 17:27:12 +0500

    Frontend pages done, Auth done, searching done

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
-import { useAuthContext } from "../../context/AuthContext";
 import { LOGOUT } from "../../constants/actionTypes";
+import { useAuthContext } from "../../context/authContext";
 import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.scss";
 
